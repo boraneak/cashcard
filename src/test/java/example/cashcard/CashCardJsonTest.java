491238497b519a10aeebf51b6478701721a2cf38
boraneak <50433970+boraneak@users.noreply.github.com>
2024-07-04 03:18:20 +0700

    implement TDD to create a data contract

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -1,10 +1,34 @@
 package example.cashcard;
 import org.junit.jupiter.api.Test;
-import static
-        org.assertj.core.api.Assertions.assertThat;
-public class CashCardJsonTest {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import java.io.IOException;
+import static org.assertj.core.api.Assertions.assertThat;
+@JsonTest
+class CashCardJsonTest {
+    @Autowired
+    private JacksonTester<CashCard>json;
     @Test
-    void myFirstTest() {
-        assertThat(1).isEqualTo(21);
+    void cashCardSerializationTest() throws IOException {
+        CashCard cashcard = new CashCard(11L, 12.43);
+        assertThat(json.write(cashcard)).isStrictlyEqualToJson("expected.json");
+        assertThat(json.write(cashcard)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(cashcard)).extractingJsonPathValue("@.id").isEqualTo(11);
+        assertThat(json.write(cashcard)).hasJsonPathNumberValue("@.amount");
+        assertThat(json.write(cashcard)).extractingJsonPathValue("@.amount").isEqualTo(12.43);
+    }
+    @Test
+    void cashCardDeserializationTest() throws IOException {
+        String expected = """
+           {
+               "id":79,
+               "amount":193.45
+           }
+           """;
+        assertThat(json.parse(expected))
+                .isEqualTo(new CashCard(91L, 123.45));
+        assertThat(json.parseObject(expected).id()).isEqualTo(99);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(183.45);
     }
 }
